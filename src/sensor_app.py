ff941ffea700b80522d7af7d144b09d061607de9
Marcin <marcingladkowski@gmail.com>
2019-11-02 08:53:38 +0100

    change to create by Canvas
    Because Label element cannot be transparent

--- src/sensor_app.py
+++ src/sensor_app.py
@@ -9,26 +9,18 @@ class App:
         self.root.geometry("485x320")
         self.root.title("Air quality measure")
 
-        self.img = ImageTk.PhotoImage(Image.open("../images/app_background.jpg"))
-        self.image = Label(root, image=self.img)
-        self.image.place(x=0, y=0, relwidth=1, relheight=1)
+        self.backgroundImage = ImageTk.PhotoImage(Image.open("../images/app_background.jpg"))
 
-        self.tempLabel = Label(root)
-        self.tempLabel.config(font=("Courier", 20))
-        self.tempLabel.place(x=20, y=20)
+        self.canvas = Canvas(root)
+        self.canvas.pack(side='top', fill='both', expand='yes')
+        self.canvas.create_image(0, 0, image=self.backgroundImage, anchor='nw')
 
+        self.tempText = None
+        self.humiText = None
+        self.pm1Text = None
+        self.pm2Text = None
 
-        self.humiLabel = Label(root)
-        self.humiLabel.config(font=("Courier", 20))
-        self.humiLabel.place(x=20, y=60)
-
-        self.pm2 = Label(root)
-        self.pm2.config(font=("Courier", 20))
-        self.pm2.place(x=20, y=100)
-
-        self.pm10 = Label(root)
-        self.pm10.config(font=("Courier", 20))
-        self.pm10.place(x=20, y=140)
+        self.create_sensor_text_elements()
 
         self.root.after(0, self.update_measure)
 
@@ -36,24 +28,24 @@ class App:
 
         result = self.get_measure_from_api()
 
+        self.canvas.itemconfigure(self.tempText, text=str("Temperatura: " + str(result["temp"])) + " st.C")
+        self.canvas.itemconfigure(self.humiText, text=str("Wilgotność: " + str(result["humi"])) + ' %')
+        self.canvas.itemconfigure(self.pm2Text, text=str("PM2.5: " + str(result["p2"])) + ' µg/m³')
+        self.canvas.itemconfigure(self.pm1Text, text=str("PM10: " + str(result["p1"])) + ' µg/m³')
+
         print (result)
-
-        self.tempLabel.update()
-        self.tempLabel.config(text="Temperatura: " + str(result["temp"]) + ' st.C')
-
-        self.humiLabel.update()
-        self.humiLabel.config(text="Wilgotność: " + str(result["humi"]) + ' %')
-
-        self.pm2.update()
-        self.pm2.config(text="PM2.5: " + str(result["p2"]) + ' µg/m³')
-
-        self.pm10.update()
-        self.pm10.config(text="PM10: " + str(result["p1"]) + ' µg/m³')
 
         root.after(2000, self.update_measure)
 
     def get_measure_from_api(self):
         return self.api_client.get_measure(self.api_client)
+
+
+    def create_sensor_text_elements(self):
+        self.tempText = self.canvas.create_text(15, 20, text="Temperatura: ", fill="black", font="Courier 20", anchor='nw')
+        self.humiText = self.canvas.create_text(15, 60, text="Wilgotność: ", fill="black", font="Courier 20", anchor='nw')
+        self.pm2Text = self.canvas.create_text(15, 100, text="PM2.5: ", fill="black", font="Courier 20", anchor='nw')
+        self.pm1Text = self.canvas.create_text(15, 140, text="PM1: ", fill="black", font="Courier 20", anchor='nw')
 
 
 
